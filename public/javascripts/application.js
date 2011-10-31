6b81891c14069b0a886a800d0ea606d717d71134
ryancaney <ryan.caney@sv.cmu.edu>
2011-10-31 01:30:46 -0400

    changed div tags

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function () {
-    if ($('#comments').length > 0) {
+    if ($('#ucomments').length > 0) {
         setTimeout(updateUcomments, 10000);
     }
 });
 
 function updateUcomments() {
     var recipe_id = $('#recipe').attr('data-id');
-    if ($('.comment').length > 0) {
-        var after = $('.comment:last').attr('data-time');
+    if ($('.ucomment').length > 0) {
+        var after = $('.ucomment:last').attr('data-time');
     }
     else {
         var after = 0;
